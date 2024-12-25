28426fde5532c9651ea881242d1de057158c41de
koustav516 <koustav516@gmail.com>
2024-12-26 00:52:33 +0530

    Add testSetup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,13 @@ export default tseslint.config(
     eslint.configs.recommended,
     tseslint.configs.recommendedTypeChecked,
     {
-        ignores: ['dist', 'node_modules', 'eslint.config.mjs'],
+        ignores: [
+            'dist',
+            'node_modules',
+            'eslint.config.mjs',
+            'jest.config.js',
+            'app.test.ts',
+        ],
     },
     {
         languageOptions: {
@@ -14,6 +20,8 @@ export default tseslint.config(
                 tsconfigRootDir: import.meta.dirname,
             },
         },
-        rules: {},
+        rules: {
+            '@typescript-eslint/no-misused-promises': 'off',
+        },
     },
 );
