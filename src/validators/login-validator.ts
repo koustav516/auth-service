694d1314a3ae20c47a3d1752117d7798cede6d60
koustav516 <koustav516@gmail.com>
2025-09-23 13:12:29 -0500

    Added login endpoint

---
+++ src/validators/login-validator.ts
@@ -0,0 +1,18 @@
+import { checkSchema } from 'express-validator';
+
+export default checkSchema({
+    email: {
+        trim: true,
+        errorMessage: 'Email is required',
+        notEmpty: true,
+        isEmail: {
+            errorMessage: 'Email should be a valid email',
+        },
+    },
+
+    password: {
+        trim: true,
+        errorMessage: 'Password required',
+        notEmpty: true,
+    },
+});
