66d88b97126dbc52032486762e9dfc354c2eeafa
koustav516 <koustav516@gmail.com>
2025-11-03 18:27:40 -0600

    Added tenant route

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { HttpError } from 'http-errors';
 import cookieParser from 'cookie-parser';
 import logger from './config/logger';
 import authRouter from './routes/auth';
+import tenantRouter from './routes/tenant';
 
 const app = express();
 
@@ -16,6 +17,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/auth', authRouter);
+app.use('/tenants', tenantRouter);
 
 //Global error handler
 app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
