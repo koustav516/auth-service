34cb8a1f9c7b99d32e0d1b4016b544dff1fe077b
koustav516 <koustav516@gmail.com>
2026-05-07 19:12:26 -0500

    Add access middleware

--- tests/tenants/create.spec.ts
+++ tests/tenants/create.spec.ts
@@ -83,6 +83,27 @@ describe('POST /tenants', () => {
             const tenants = await tenantRepo.find();
             expect(tenants).toHaveLength(0);
         });
+
+        it('should return 403 if user is not an admin', async () => {
+            const managerToken = jwks.token({
+                sub: '1',
+                role: Roles.MANAGER,
+            });
+            const tenantData = {
+                name: 'Tenant Name',
+                address: 'Tenant Address',
+            };
+
+            const response = await request(app)
+                .post('/tenants')
+                .set('Cookie', [`accessToken=${managerToken}`])
+                .send(tenantData);
+            expect(response.statusCode).toBe(403);
+
+            const tenantRepo = connection.getRepository(Tenant);
+            const tenants = await tenantRepo.find();
+            expect(tenants).toHaveLength(0);
+        });
     });
 
     describe('Missing fields', () => {});
